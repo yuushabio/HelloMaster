ad3c79e9cb9bfd16823645226347bcd8e68be78a
mattadd007 <matt007@gmail.gh>
2017-01-29 14:21:27 -0800

    kuma editing

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
     cout <<"can you lend me some money"<<endl;
     cout <<"How do feel?" << endl;
     cout<<"well,it worked"<<endl;
+    cout<< "tec store app is the best";
 
     return 0;
 }
