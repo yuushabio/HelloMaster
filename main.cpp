28bade63babf285057889ae0c12f5339769a7cdd
Noah <freemannero@gmail.com>
2017-02-09 21:19:25 +0000

    Group meeting report

--- main.cpp
+++ main.cpp
@@ -9,6 +9,12 @@ int main()
     cout <<"can you lend me some money"<<endl;
     cout <<"How do feel?" << endl;
     cout<<"well,it worked"<<endl;
+<<<<<<< Updated upstream
     cout<< "tec store app is the best";// Ansu gyeabour kwabena commit 
+=======
+    cout<< "tec store app is the best";
+    cout<<"GitKraken test"<<endl;
+
+>>>>>>> Stashed changes
     return 0;
 }
